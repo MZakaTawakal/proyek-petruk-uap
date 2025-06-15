4cd5f84ad51c619e554ccd3c5012e2d5e6420962
RaihanSM <83532862+RaihanSM@users.noreply.github.com>
2025-06-15 13:13:24 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ struct Stats {
 class Territory {
 public:
     char name[20];
-    Stats stats; 
+    Stats stats;
     Territory* left;
     Territory* right;
 
@@ -27,19 +27,56 @@ public:
     }
 };
 
-void map(){
-    cout<<"North1    North2"<<endl;
-    cout<<"  \\         /"<<endl;
-    cout<<"   \\       /"<<endl;
-    cout<<"    \\     /"<<endl;
-    cout<<"      East-------------Kingdom-------------West"<<endl;
-    cout<<"                                           /   \\"<<endl;
-    cout<<"                                          /     \\"<<endl;
-    cout<<"                                         /       \\"<<endl;
-    cout<<"                                      South1    South2"<<endl;
+Territory* conquerAndCombine(Territory* root, vector<string>& log) {
+    if (!root) return nullptr;
+
+    Territory* left = conquerAndCombine(root->left, log);
+    Territory* right = conquerAndCombine(root->right, log);
+
+    bool conqueredLeft = true, conqueredRight = true;
+
+    if (left && left->stats.strength > root->stats.strength) {
+        log.push_back(string(root->name) + " gagal menaklukkan " + left->name);
+        conqueredLeft = false;
+    }
+    if (right && right->stats.strength > root->stats.strength) {
+        log.push_back(string(root->name) + " gagal menaklukkan " + right->name);
+        conqueredRight = false;
+    }
+
+    if (conqueredLeft && left) {
+        log.push_back(string(root->name) + " menggabungkan " + left->name);
+        root->stats.population += left->stats.population;
+        root->stats.strength += left->stats.strength;
+    }
+    if (conqueredRight && right) {
+        log.push_back(string(root->name) + " menggabungkan " + right->name);
+        root->stats.population += right->stats.population;
+        root->stats.strength += right->stats.strength;
+    }
+
+    return root;
 }
 
 int main() {
-    map();  // hanya menampilkan peta
+    vector<string> logHasil;
+
+    Territory* A = new Territory("A", 100, 30);
+    Territory* B = new Territory("B", 150, 40);
+    Territory* Root = new Territory("Root", 300, 50);
+
+    Root->left = A;
+    Root->right = B;
+
+    conquerAndCombine(Root, logHasil);
+
+    for (const string& log : logHasil) {
+        cout << log << endl;
+    }
+
+    delete A;
+    delete B;
+    delete Root;
+
     return 0;
 }
