fb30a1911c2a236cd8e05c015e4f71374f79cf0d
Habib <112567198+MuhammadHabibullahAlHikam@users.noreply.github.com>
2025-06-15 13:03:21 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <cstring> 
 using namespace std;
+
+struct Stats {
+    int population;
+    int strength;
+};
+
+class Territory {
+public:
+    char name[20];
+    Stats stats; 
+    Territory* left;
+    Territory* right;
+
+    Territory(const char* n, int pop, int str) : left(nullptr), right(nullptr) {
+        strcpy(name, n);
+        stats.population = pop;
+        stats.strength = str;
+    }
+};
+
+void map(){
+    cout<<"North1    North2"<<endl;
+    cout<<"  \\         /"<<endl;
+    cout<<"   \\       /"<<endl;
+    cout<<"    \\     /"<<endl;
+    cout<<"      East-------------Kingdom-------------West"<<endl;
+    cout<<"                                           /   \\"<<endl;
+    cout<<"                                          /     \\"<<endl;
+    cout<<"                                         /       \\"<<endl;
+    cout<<"                                      South1    South2"<<endl;
+}
+
 
 struct Territory {
     string name;
@@ -74,6 +111,7 @@ Territory* createSampleWorld() {
 }
 
 int main() {
+    map();
     Territory* world = createSampleWorld();
     cout << "Memulai proses penaklukan dan penggabungan wilayah...\n\n";
     conquerAndCombine(world);
